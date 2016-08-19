c4a446bde46ab3cae81a9d68a1f7d0da76941a27
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-08-19 22:49:56 +1200

    Add webpack server

---
+++ server.js
@@ -0,0 +1,18 @@
+const webpack = require( 'webpack' );
+const WebpackDevServer = require( 'webpack-dev-server' );
+const config = require( './webpack.config' );
+
+new WebpackDevServer(webpack( config ), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback:true,
+  stats: {
+    colors: true
+  }
+}).listen( 8000, 'localhost', ( err ) => {
+  if ( err ) {
+    console.log( err );
+  }
+
+  console.log( 'Listening to localhost:8000' );
+});
