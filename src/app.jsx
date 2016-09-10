aeb705d405b2b8f1517485a1bfc35a7f7b8d77f2
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-09-10 23:07:58 +1200

    Lint app

--- src/app.jsx
+++ src/app.jsx
@@ -1,67 +1,69 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {
-  handleLoginResponse
+  handleLoginResponse,
 } from './actions';
 
 // import AppBar from 'material-ui/lib/app-bar';
 
 import { Row, Col } from 'elemental';
 
-const App  = React.createClass({
+const App = React.createClass( {
   displayName: 'LoginPage',
+
   componentDidMount() {
-    window.fbAsyncInit = function() {
-      FB.init({
+    window.fbAsyncInit = () => {
+      FB.init( {
         appId: '1696899793858404',
         xfbml: true,
-        version: 'v2.5'
-      });
+        version: 'v2.5',
+      } );
 
-      FB.getLoginStatus(function( response ) {
+      FB.getLoginStatus( ( response ) => {
         this.statusChangeCallback( response );
-      }.bind( this ) );
-    }.bind( this );
+      } );
+    };
 
-    (function( d, s, id ) {
-       var js, fjs = d.getElementsByTagName( s )[ 0 ];
-       if ( d.getElementById( id ) ) {
-         return;
-       }
-       js = d.createElement( s );
-       js.id = id;
-       js.src = '//connect.facebook.net/en_US/sdk.js';
-       fjs.parentNode.insertBefore( js, fjs );
-     }( document, 'script', 'facebook-jssdk' ));
+    ( ( d, s, id ) => {
+      const fjs = d.getElementsByTagName( s )[0];
+      let js = null;
+      if ( d.getElementById( id ) ) {
+        return;
+      }
+      js = d.createElement( s );
+      js.id = id;
+      js.src = '//connect.facebook.net/en_US/sdk.js';
+      fjs.parentNode.insertBefore( js, fjs );
+    } )( document, 'script', 'facebook-jssdk' );
   },
+
   statusChangeCallback( response ) {
     const { dispatch } = this.props;
     handleLoginResponse( dispatch )( response );
   },
+
   render() {
     return (
       <div>
         <Row>
-          <Col xs='33%' sm='25%' lg='33.333%'></Col>
-          <Col xs='33%' sm='25%' lg='33.333%'>
+          <Col xs="33%" sm="25%" lg="33.333%" />
+          <Col xs="33%" sm="25%" lg="33.333%">
             <h1>Voyeur Cam</h1>
             { this.props.children }
           </Col>
-          <Col xs='33%' sm='25%' lg='33.333%'></Col>
+          <Col xs="33%" sm="25%" lg="33.333%" />
         </Row>
 
       </div>
     );
-  }
-});
+  },
+} );
 
 function mapStateToProps( state ) {
-  const {
-    auth
-  } = state;
+  const { auth } = state;
 
   return {
-    auth
+    auth,
   };
 }
 
