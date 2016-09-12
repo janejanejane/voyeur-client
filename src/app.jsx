862c20fb411c70efe9a5f4d6773159d834a3c91a
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-09-12 23:00:32 +1200

    Update app

--- src/app.jsx
+++ src/app.jsx
@@ -11,37 +11,6 @@ import { Row, Col } from 'elemental';
 const App = React.createClass( {
   displayName: 'LoginPage',
 
-  componentDidMount() {
-    window.fbAsyncInit = () => {
-      FB.init( {
-        appId: '1696899793858404',
-        xfbml: true,
-        version: 'v2.5',
-      } );
-
-      FB.getLoginStatus( ( response ) => {
-        this.statusChangeCallback( response );
-      } );
-    };
-
-    ( ( d, s, id ) => {
-      const fjs = d.getElementsByTagName( s )[0];
-      let js = null;
-      if ( d.getElementById( id ) ) {
-        return;
-      }
-      js = d.createElement( s );
-      js.id = id;
-      js.src = '//connect.facebook.net/en_US/sdk.js';
-      fjs.parentNode.insertBefore( js, fjs );
-    } )( document, 'script', 'facebook-jssdk' );
-  },
-
-  statusChangeCallback( response ) {
-    const { dispatch } = this.props;
-    handleLoginResponse( dispatch )( response );
-  },
-
   render() {
     return (
       <div>
