d09e463fd2ace99cde2268321bf9fb152b6f0640
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-09-15 22:29:50 +1200

    Add sample lifecycle test

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import { expect } from 'chai';
+import { spy } from 'sinon';
+
+import App from '../../app';
 
 describe( 'app', () => {
-  it( 'should test', () => {
-    expect( true ).to.be.true;
+  describe( 'componentWillMount', () => {
+    it( 'loads once', () => {
+      const component = spy( App.prototype, 'componentDidMount' );
+      mount( <App /> );
+
+      expect( component.calledOnce ).to.equal( true );
+    } );
   } );
 } );
