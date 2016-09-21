463310bac6ada51dea00b6894a47f5e6a301fa0b
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-09-21 22:51:40 +1200

    Update typo

--- test/pages/home2.spec.js
+++ test/pages/home2.spec.js
@@ -6,12 +6,9 @@ import Home from '../../src/pages/home';
 
 describe( '<Home />', () => {
   describe( 'render', () => {
-    it( 'should have a button element', () => {
-
     it( 'should have an image element', () => {
       const wrapper = shallow( <Home /> );
       expect( wrapper.find( 'img' ) ).to.have.length( 1 );
     } );
-
   } );
 } );
